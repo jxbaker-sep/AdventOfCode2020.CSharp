089136b468add826ab155e9269b3ca120da51b95
Jake Baker <SEP.Baker@fronius.com>
2025-01-29 12:22:39 -0500

    day14.2

--- Day14.cs
+++ Day14.cs
@@ -34,6 +34,50 @@ public class Day14
     cache.Values.Sum().Should().Be(expected);
   }
 
+  [Theory]
+  [InlineData("Day14.Sample.2", 208)]
+  [InlineData("Day14", 3618217244644)]
+  public void Part2(string file, long expected)
+  {
+    var program = Convert(AoCLoader.LoadLines(file));
+    
+    var mask = "";
+    Dictionary<long, long> cache = [];
+    foreach(var item in program)
+    {
+      if (item is MaskInstruction mi)
+      {
+        mask = mi.Mask.Reverse().Join();
+      }
+      else if (item is MemInstruction mem)
+      {
+        foreach(var address in GetAddresses(mask, mem.Address))
+        {
+          cache[address] = mem.Value;
+        }
+      }
+    }
+
+    cache.Values.Sum().Should().Be(expected);
+  }
+
+  private static IEnumerable<long> GetAddresses(string mask, long address)
+  {
+    if (mask.Length == 0) {
+      yield return 0; 
+      yield break;
+    }
+    foreach(var remainder in GetAddresses(mask[1..], address >> 1))
+    {
+      if (mask[0] == '0') yield return (remainder << 1) + (address & 1);
+      if (mask[0] == '1') yield return (remainder << 1) + 1;
+      if (mask[0] == 'X') { 
+        yield return (remainder << 1) + 0;
+        yield return (remainder << 1) + 1;
+      }
+    }
+  }
+
   public static long DoMask(string mask, long value)
   {
     var result = 0L;
