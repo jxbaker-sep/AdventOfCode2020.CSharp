9ed4a6f8b206ff8c39091ccbf8aeea42a40303e5
Jake Baker <SEP.Baker@fronius.com>
2025-01-29 11:40:56 -0500

    day 13 part 2 via crt via euclids algorithm, had to use BigIntegers.

--- Day13.cs
+++ Day13.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using AdventOfCode2020.CSharp.Utils;
 using FluentAssertions;
 using Microsoft.VisualBasic;
@@ -5,8 +6,11 @@ using Parser;
 using Utils;
 using P = Parser.ParserBuiltins;
 
-
 namespace AdventOfCode2020.CSharp;
+
+
+
+
 
 public class Day13
 {
@@ -26,11 +30,25 @@ public class Day13
   [Theory]
   [InlineData("Day13.Sample", 1068781)]
   [InlineData("Day13", 600689120448303)]
-  public void Part2(string file, int expected)
+  public void Part2ViaSieve(string file, long expected)
   {
     var (timestamp, schedule) = Convert(AoCLoader.LoadLines(file));
 
     Sieve(schedule.Select((id, index) => (id, index))
+      .Where(it => it.id != -1)
+      .Select(it => (remainder: MathMod(it.id - it.index, it.id), modulus: it.id))
+      .ToList())
+    .Should().Be(expected);
+  }
+
+  [Theory]
+  [InlineData("Day13.Sample", 1068781)]
+  [InlineData("Day13", 600689120448303)]
+  public void Part2ViaCRT(string file, long expected)
+  {
+    var (timestamp, schedule) = Convert(AoCLoader.LoadLines(file));
+
+    CRT(schedule.Select((id, index) => (id, index))
       .Where(it => it.id != -1)
       .Select(it => (remainder: MathMod(it.id - it.index, it.id), modulus: it.id))
       .ToList())
@@ -42,9 +60,58 @@ public class Day13
     return ((a % b) + b) % b;
   }
 
-  static long Sieve(List<(long remainder, long modulus)> r) // 
+  private static BigInteger MathMod(BigInteger a, BigInteger b)
   {
-    r = r.OrderByDescending(it => it.modulus).ToList();
+    return ((a % b) + b) % b;
+  }
+
+  [Fact]
+  public void CrtTest()
+  {
+    CRT([(0, 3), (3, 4), (4,5)]).Should().Be(39);
+  }
+
+  static long CRT(List<(long remainder, long modulus)> r)
+  {
+    checked {
+    r = [.. r.OrderByDescending(it => it.modulus)];
+    var current = r[0];
+    foreach(var next in r[1..])
+    {
+      var ec = ExtendedEuclidAlgorithm(current.modulus, next.modulus);
+      var vex = new BigInteger(next.remainder) * ec.coeffA * current.modulus + new BigInteger(current.remainder) * ec.coeffB * next.modulus;
+      vex = MathMod(vex, current.modulus * next.modulus);
+      current = ((long)vex, current.modulus * next.modulus);
+    }
+
+    return current.remainder;
+    }
+  }
+
+  private static (long gcd, long coeffA, long coeffB) ExtendedEuclidAlgorithm(long a, long b)
+  {
+    // requirement a > b
+    long r2 = a;
+    long r1 = b;
+    long s2 = 1;
+    long s1 = 0;
+    long t2 = 0;
+    long t1 = 1;
+
+    while (r1 != 0)
+    {
+      var q = r2 / r1;
+      (r2, r1) = (r1, r2 - q * r1);
+      (s2, s1) = (s1, s2 - q * s1);
+      (t2, t1) = (t1, t2 - q * t1);
+    }
+
+    return (r2, s2, t2);
+  }
+
+  static long Sieve(List<(long remainder, long modulus)> r)
+  {
+    r = [.. r.OrderByDescending(it => it.modulus)];
     long increment = r[0].modulus;
     var current = r[0].remainder;
     var n = 1;
