f3d021a26e1fb7ca9eefdabf7b591aef9dc018e8
Jake Baker <SEP.Baker@fronius.com>
2025-01-30 14:55:25 -0500

    day17.2

--- Day17.cs
+++ Day17.cs
@@ -34,6 +34,30 @@ public class Day17
     grid.Count.Should().Be(expected);
   }
 
+  [Theory]
+  [InlineData("Day17.Sample", 848)]
+  [InlineData("Day17", 2292)]
+  public void Part2(string file, int expected)
+  {
+    var grid = Convert(AoCLoader.LoadLines(file)).Select(it => new Point4(it.X, it.Y, 0, 0)).ToHashSet();
+    
+    foreach(var _ in Enumerable.Repeat(0, 6)) {
+      var weights = grid.SelectMany(it => it.Neighbors()).GroupToDictionary(it => it, it => it, it => it.Count);
+
+      HashSet<Point4> next = [];
+      foreach(var cube in weights.Keys.Union(grid)) {
+        var weight = weights.GetValueOrDefault(cube);
+        if (grid.Contains(cube)) {
+          if (weight == 2 || weight == 3) next.Add(cube);
+        }
+        else if (weight == 3) next.Add(cube);
+      }
+      grid = next;
+    }
+
+    grid.Count.Should().Be(expected);
+  }
+
   private static HashSet<Point3> Convert(List<string> input) => input.Gridify()
     .Where(it => it.Value == '#')
     .Select(it => it.Key)
@@ -43,10 +67,16 @@ public class Day17
 
 public record Point3(long X, long Y, long Z)
 {
-  public long ManhattanDistance(Point3 other) => Math.Abs(X - other.X) + Math.Abs(Y - other.Y) + Math.Abs(Z - other.Z);
-
   public IEnumerable<Point3> Neighbors() {
     List<long> zed = [-1,0,1];
     foreach(var dx in zed) foreach(var dy in zed) foreach(var dz in zed) if (dz != 0 || dy != 0 || dx != 0) yield return new Point3(X + dx, Y + dy, Z + dz);
+  }
+}
+
+public record Point4(long X, long Y, long Z, long W)
+{
+  public IEnumerable<Point4> Neighbors() {
+    List<long> zed = [-1,0,1];
+    foreach(var dx in zed) foreach(var dy in zed) foreach(var dz in zed) foreach(var dw in zed) if (dz != 0 || dy != 0 || dx != 0 || dw != 0) yield return new Point4(X + dx, Y + dy, Z + dz, W + dw);
   }
 }
