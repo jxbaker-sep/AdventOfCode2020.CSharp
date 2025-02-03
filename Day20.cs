c2bc1e58879470422de14443d7396b9fe5022dda
Jake Baker <SEP.Baker@fronius.com>
2025-02-03 08:18:22 -0500

    Turned matched into list of ids

--- Day20.cs
+++ Day20.cs
@@ -17,24 +17,28 @@ public class Day20
   {
     var images = Convert(AoCLoader.LoadFile(file));
 
+    // Sanity check: every image has a unique set of keys
+    images.All(image => image.LL.SelectMany(it => it).Distinct().Count() == image.LL.SelectMany(it => it).Count())
+      .Should().BeTrue();
+
     var legals = Alignments(images.ToDictionary(it => it.Id, it => it))
       .Where(Legal).Select(Score).First().Should().Be(expected);
   }
 
   static long Score(Dictionary<long, Image> images)
   {
-    return images.Where(image => image.Value.Matched.Count(it => it) == 2).Select(it => it.Key)
+    return images.Where(image => image.Value.Matched.Count(it => it != null) == 2).Select(it => it.Key)
       .Aggregate((a,b) => a*b);
 
   }
 
   static bool Legal(Dictionary<long, Image> images) {
-    var invalids = images.Values.Count(image => image.Matched.Count(it => it) < 2);
+    var invalids = images.Values.Count(image => image.Matched.Count(it => it != null) < 2);
     if (invalids > 0) return false;
-    var twos = images.Values.Count(image => image.Matched.Count(it => it) == 2);
+    var twos = images.Values.Count(image => image.Matched.Count(it => it != null) == 2);
     if (twos != 4) return false;
-    var threes = images.Values.Count(image => image.Matched.Count(it => it) == 3);
-    var fours = images.Values.Count(image => image.Matched.Count(it => it) == 4);
+    var threes = images.Values.Count(image => image.Matched.Count(it => it != null) == 3);
+    var fours = images.Values.Count(image => image.Matched.Count(it => it != null) == 4);
     
     var sideLength = images.Count == 9 ? 3 : images.Count == 144 ? 12 : throw new ApplicationException();
     if (threes != (sideLength-2) * 4) return false;
@@ -49,20 +53,20 @@ public class Day20
     }
     var m1 = images.First().Value;
     var atLeastOne = false;
-    foreach(var ll1 in Enumerable.Range(0, 4).Where(it => !m1.Matched[it]))
+    foreach(var ll1 in Enumerable.Range(0, 4).Where(it => m1.Matched[it] == null))
     {
       foreach(var other in images.Values.Where(it => it.Id != m1.Id) )
       {
-        foreach(var ll2 in Enumerable.Range(0, 4).Where(it => !other.Matched[it]))
+        foreach(var ll2 in Enumerable.Range(0, 4).Where(it => other.Matched[it] == null))
         {
           if (m1.LL[ll1].Intersect(other.LL[ll2]).Any())
           {
             atLeastOne = true;
             var clone = images.Clone();
-            List<bool> mm1 = [..m1.Matched];
-            List<bool> mm2 = [..other.Matched];
-            mm1[ll1] = true;
-            mm2[ll2] = true;
+            List<long?> mm1 = [..m1.Matched];
+            List<long?> mm2 = [..other.Matched];
+            mm1[ll1] = other.Id;
+            mm2[ll2] = m1.Id;
             clone[m1.Id] = m1 with {Matched = mm1};
             clone[other.Id] = other with {Matched = mm2};
             foreach(var next in Alignments(clone)) yield return next;
@@ -82,7 +86,7 @@ public class Day20
     }
   }
 
-  public record Image(long Id, List<List<char>> Data, IReadOnlyList<bool> Matched)
+  public record Image(long Id, List<List<char>> Data, IReadOnlyList<long?> Matched)
   {
     public List<List<string>> LL = [Get(Data[0]), Get(Data.Select(it => it[^1]).ToList()), Get(Data[^1]), Get(Data.Select(it => it[0]).ToList())];
 
@@ -99,6 +103,6 @@ public class Day20
   {
     var pps = input.Split("\n\n").Select(it => it.Split("\n").ToList()).ToList();
 
-    return pps.Select(pp => new Image(P.Format("Tile {}:", P.Long).Parse(pp[0]), pp[1..].Select(it => it.ToCharArray().ToList()).ToList(), [false, false, false, false])).ToList();
+    return pps.Select(pp => new Image(P.Format("Tile {}:", P.Long).Parse(pp[0]), pp[1..].Select(it => it.ToCharArray().ToList()).ToList(), [null, null, null, null])).ToList();
   }
 }
