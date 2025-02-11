b4d94f49d2f09179704f4fc0adadba7995233cfb
Jake Baker <SEP.Baker@fronius.com>
2025-02-11 14:15:17 -0500

    day24.1

--- Day24.cs
+++ Day24.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks.Dataflow;
 using AdventOfCode2020.CSharp.Utils;
 using FluentAssertions;
 using Parser;
@@ -10,16 +11,45 @@ namespace AdventOfCode2020.CSharp;
 public class Day24
 {
   [Theory]
-  [InlineData("Day24.Sample", 10)]
-  [InlineData("Day24", 312)]
-  public void Part1(string file, int expected)
+  [InlineData("Day24.Sample", 10, 2208)]
+  [InlineData("Day24", 312, 0)]
+  public void Part1(string file, int expected1, int expected2)
   {
     var instructions = Convert(AoCLoader.LoadLines(file));
 
     var hexToFlips = instructions.Select(it => it.Aggregate(Hex.Zero, (prev, next) => prev + next))
       .GroupToDictionary(it => it, it => it, it => it.Count);
 
-    hexToFlips.Count(it => it.Value % 2 == 1).Should().Be(expected);
+    var blackHexes = hexToFlips.Where(it => it.Value % 2 == 1).Select(it => it.Key).ToHashSet();
+
+    blackHexes.Count.Should().Be(expected1);
+
+    foreach(var _ in Enumerable.Range(0, 100))
+    {
+      var counts = blackHexes.SelectMany(it => Open(it)).GroupToDictionary(it => it, it => it, it => it.Count);
+      HashSet<Hex> next = [];
+      foreach(var hex in blackHexes) 
+      {
+        var count = counts.GetValueOrDefault(hex);
+        if (count == 1) next.Add(hex);
+      }
+      foreach(var hex in counts.Keys.Except(blackHexes))
+      {
+        var count = counts.GetValueOrDefault(hex);
+        if (count == 2) next.Add(hex);
+      }
+      blackHexes = next;
+    }
+    blackHexes.Count.Should().Be(expected2);
+  }
+
+  static IEnumerable<Hex> Open(Hex me) {
+    yield return me + HexVector.North;
+    yield return me + HexVector.NorthWest;
+    yield return me + HexVector.NorthEast;
+    yield return me + HexVector.South;
+    yield return me + HexVector.SouthEast;
+    yield return me + HexVector.SouthWest;
   }
 
   private static List<List<HexVector>> Convert(List<string> input)
